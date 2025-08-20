2fdcf2483ba6d6b76e905d9a4e45f1afb4030abc
Mehmet Aşar Kıribiş <mehmet_asar_@hotmail.com>
2025-08-20 11:09:07 +0300

    Revize edildi.

--- queries.sql
+++ queries.sql
@@ -1,27 +1,16 @@
 -- 1. film tablosunda title ve description sütunları
-SELECT title, description
-FROM film
-ORDER BY title, description;
+SELECT title, description FROM film
 
 -- 2. film tablosu length 60 < length < 75
-SELECT *
-FROM film
-WHERE length > 60 AND length < 75
-ORDER BY length;
+select * from film where length>60 and length <75;
 
 -- 3. rental_rate ve replacement_cost koşulu
-SELECT *
-FROM film
-WHERE rental_rate = 0.99
-  AND replacement_cost IN (12.99, 28.99);
+select * from film where rental_rate = 0.99 and (replacement_cost = 12.99 or replacement_cost = 28.99);
+
 
 -- 4. first_name 'Mary' olan customer’ın last_name değeri
-SELECT last_name
-FROM customer
-WHERE first_name ILIKE 'Mary';
+select last_name from customer  WHERE first_name = 'Mary';
+
 
 -- 5. length ≤ 50 ve rental_rate ≠ 2.99, 4.99
-SELECT *
-FROM film
-WHERE length <= 50
-  AND rental_rate NOT IN (2.99, 4.99);
+select * from film where length <= 50 and (rental_rate = 2.99 or rental_rate=4.99);
